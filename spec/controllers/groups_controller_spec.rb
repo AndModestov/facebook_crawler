eac11134beae9fe99c4485fa313701d698015f8d
AndModestov <and.modestov@gmail.com>
2016-06-24 19:35:58 +0300

    specs addition

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,32 +2,84 @@ require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe 'GET #index' do
+    let(:groups){ create_list(:group, 2) }
+    before { get :index}
+
+    it 'assigns a @groups list' do
+      expect(assigns(:groups)).to eq groups
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    let(:group) { create(:group) }
+    before { get :show, id: group }
+
+    it 'assigns the requested group to @group' do
+      expect(assigns(:group)).to eq group
+    end
+
+    it 'should render the show view' do
+      expect(response).to render_template :show
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new group to @group' do
+      expect(assigns(:group)).to be_a_new(Group)
+    end
+
+    it 'should render the new view' do
+      expect(response).to render_template :new
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    context 'with valid information' do
+      it 'saves the group in database' do
+        expect {
+          post :create, group: attributes_for(:group)
+        }.to change(Group, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, group: attributes_for(:group)
+        expect(response).to redirect_to assigns(:group)
+      end
+    end
+
+    context 'with invalid information' do
+      it 'does not save the group' do
+        expect {
+          post :create, group: attributes_for(:invalid_group)
+        }.to_not change(Group, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, group: attributes_for(:invalid_group)
+        expect(response).to render_template :new
+      end
     end
   end
 
+  describe 'DELETE #destroy' do
+    let!(:group) { create(:group) }
+
+    it 'deletes group' do
+      expect{
+        delete :destroy, id: group
+      }.to change(Group, :count).by(-1)
+    end
+
+    it 'redirect to index view' do
+      delete :destroy, id: group
+      expect(response).to redirect_to groups_path
+    end
+  end
 end
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     sequence(:name) { |n| "group-#{n}" }
     url 'http://example.com'
   end
+
+  factory :invalid_group, class: 'Group' do
+    sequence(:name) { |n| "group-#{n}" }
+    url nil
+  end
 end
